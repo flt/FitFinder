5ccc6ac596eb69dd588693a9471a9d220d7faf6a
冯乐桐 <fengletong10@gmail.com>
2016-07-02 13:01:16 +0800

    all info

--- application/secret_keys.py
+++
@@ -1,4 +0,0 @@
-# CSRF- and Session keys
-
-CSRF_SECRET_KEY = 'KJuak2kLzjZlneztrXVk38eZ'
-SESSION_KEY = 'agoAmgR4hv4w2a8knXT0app6'
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! This is greeting from FitFinder~~~'
+    return 'Hello World!'
 
 
 @app.errorhandler(404)
